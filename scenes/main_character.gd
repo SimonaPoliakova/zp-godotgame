04eadd6a7bfbed9bc54700ea536f8d8d2ad74192
SimonaPoliakova <poliakova27712@gmail.com>
2025-02-05 01:53:15 +0100

    player movement and level scenes

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -790.0
+const JUMP_VELOCITY = -780.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+var last_direction = 1  # 1 = right, -1 = left
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity
@@ -10,20 +12,21 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle movement
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		last_direction = direction  # Save last movement direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
 
-	# Flip sprite based on direction
-	sprite_2d.flip_h = velocity.x < 0
+	# Flip the sprite based on last direction
+	sprite_2d.flip_h = last_direction < 0  
 
 	# ANIMATION LOGIC
 	if not is_on_floor():
@@ -31,7 +34,7 @@ func _physics_process(delta: float) -> void:
 	elif abs(velocity.x) > 1:
 		sprite_2d.animation = "running"
 	else:
-		sprite_2d.animation = "default"
+		sprite_2d.animation = "default"  # Use the same idle animation, just flipped
 
 	# Play the selected animation
 	sprite_2d.play()
